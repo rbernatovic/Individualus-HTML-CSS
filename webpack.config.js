53062916fbb3a321d8b8b25bbc9929e290d67421
rbernatovic <57090199+rbernatovic@users.noreply.github.com>
2020-02-13 21:24:29 +0200

    individualus projektas

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
+const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -24,13 +26,50 @@ module.exports = {
         test: /\.(png|svg|jpg|gif|webp)$/,
         loader: 'file-loader',
         options: {
-          publicPath: '../',
-          name: 'images/[name].[ext]',
+          outputPath: 'assets/images',
+          publicPath: '../images',
+          name: '[name].[ext]',
         },
-      }
+      },
+      { test: /\.hbs$/, loader: "handlebars-loader" }
     ],
   },
   plugins: [
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        handlebarsLoader: {}
+      }
+    }),
+    new HtmlWebpackPlugin({
+      title: 'index',
+      filename: 'index.html',
+      template: './src/index.hbs'
+    }),
+    new HtmlWebpackPlugin({
+      title: 'about',
+      filename: 'about.html',
+      template: './src/about.hbs'
+    }),
+    new HtmlWebpackPlugin({
+      title: 'services',
+      filename: 'services.html',
+      template: './src/services.hbs'
+    }),
+    new HtmlWebpackPlugin({
+      title: 'plans',
+      filename: 'plans.html',
+      template: './src/plans.hbs'
+    }),
+    new HtmlWebpackPlugin({
+      title: 'contact',
+      filename: 'contact.html',
+      template: './src/contact.hbs'
+    }),
+    new HtmlWebpackPlugin({
+      title: 'blog',
+      filename: 'blog.html',
+      template: './src/blog.hbs'
+    }),
     new MiniCssExtractPlugin({
       filename: 'assets/styles/app.css',
     }),
